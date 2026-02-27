73bbd54fd8a5c7e778b6909dd5b0736d5c386efe
KG070324 <zgpn0703@leeds.ac.uk>
2026-02-27 00:16:18 +0000

    completed portfolio

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Kardo Ghaffouri
+Student ID: zgpn0703
 */
 
 #include <stdio.h>
@@ -10,10 +10,20 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char mainString[100] = ""; // 100 is safe amount 
+
 
     // process the command-line data using appropriate string functions
+    for (int i = 1; i < argc; i++){
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+        strcat(mainString, argv[i]); // append curent arg
+
+        if (i < argc - 1){ // if not last arg the append hyphen
+            strcat(mainString, "-"); 
+        }
+    }
+
+    printf("%s\n",mainString); // use only this single print statement in your submitted code
 
     return 0;
 }
